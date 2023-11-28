8520efb2ef5a07351f3be1aa51cac95b833057db
CoderRoy05 <royalalawode@gmail.com>
2023-11-28 13:58:09 +0100

    updated features on routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx';
 
-const rootElement = document.getElementById('root');
+// const rootElement = document.getElementById('root');
+  // not 
+// // Use createRoot to enable concurrent rendering
+ //  not 
+// const root = ReactDOM.createRoot(rootElement);
 
-// Use createRoot to enable concurrent rendering
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
+// root.render(
+//   <React.StrictMode>
+//     <Router>
+//       <App />
+//     </Router>
+//   </React.StrictMode>
+// );
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
